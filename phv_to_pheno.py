55fe50ffe76535e5305f3f421a88d303946bd999
Anne Thessen <annethessen@gmail.com>
2019-02-06 11:06:51 -0500

    new results based on updated topmed data

--- phv_to_pheno.py
+++ phv_to_pheno.py
@@ -1,8 +1,9 @@
 import pickle
+import re
 
 #opening input files and dictionaries
-infile = open('2018-10-05_1214_tagged_variables.txt', 'r')
-url_dict = pickle.load(open('topmed_urls.p', 'rb'))
+infile = open('2018-12-28_1202_tagged_variables.txt', 'r')
+#url_dict = pickle.load(open('topmed_urls.p', 'rb'))
 curies = open('curie_table.tsv', 'r')
 
 #create output file
@@ -15,10 +16,13 @@ for line in infile: #iterate over infile
 	row = line.split('\t')
 	tag_id = row[0]
 	variable_phv = row[2]
-	if variable_phv in url_dict:
-		x,y = url_dict[variable_phv].split('|') #look up variable_phv in dictionary to get data set and study URLs
-	else:
-		continue
+	variable_acc = row[3]
+	set_acc = row[4]
+	m = re.match('\w\w\w[\d]+', set_acc)
+	set_id = m[0][-4:]
+	study_acc = row[5]
+	set_url = 'https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bin/study.cgi?study_id=' + study_acc 
+	study_url = 'https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bin/dataset.cgi?study_id=' + study_acc + '&pht=' + set_id
 	curies.seek(0)	
 	for line in curies: #look for phenotype in ontology map
 		row = line.split('\t')
@@ -29,6 +33,6 @@ for line in infile: #iterate over infile
 			mondo = row[10]
 			efo = row[13]
 			oba = row[16]
-			out_file.write('\t'.join([variable_phv,tag_id,ncit,hpo,mondo,efo,oba,x,y]) + '\n') #output mapping
+			out_file.write('\t'.join([variable_phv,tag_id,ncit,hpo,mondo,efo,oba,set_url,study_url]) + '\n') #output mapping
 		else:
 			continue
