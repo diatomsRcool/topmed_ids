8d8b076dcdc4063c94fd8d90477d7e04f4ba5c51
Anne Thessen <annethessen@gmail.com>
2019-02-26 09:51:05 -0500

    outputting accession id instead of url

--- phv_to_pheno.py
+++ phv_to_pheno.py
@@ -3,13 +3,15 @@ import re
 
 #opening input files and dictionaries
 infile = open('2018-12-28_1202_tagged_variables.txt', 'r')
+#since the new infile has the topmed ids, we don't need the url_dict
 #url_dict = pickle.load(open('topmed_urls.p', 'rb'))
 curies = open('curie_table.tsv', 'r')
 
 #create output file
 out_file = open('topmed_curies.tsv', 'w')
 #add column headers to output file
-out_file.write('variable_phv	tag_id	ncit	hpo	mondo	efo	oba	data_set	study\n')
+#out_file.write('variable_phv	tag_id	ncit	hpo	mondo	efo	oba	data_set	study\n')
+out_file.write('variable_phv	tag_id	ncit	hpo	mondo	efo	oba	variable	data_set	study\n')
 
 next(infile)
 for line in infile: #iterate over infile
@@ -18,11 +20,14 @@ for line in infile: #iterate over infile
 	variable_phv = row[2]
 	variable_acc = row[3]
 	set_acc = row[4]
-	m = re.match('\w\w\w[\d]+', set_acc)
-	set_id = m[0][-4:]
+	#m = re.match('\w\w\w[\d]+', set_acc) #only need to manipulate set_acc if using urls
+	#set_id = m[0][-4:]
 	study_acc = row[5]
-	set_url = 'https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bin/study.cgi?study_id=' + study_acc 
-	study_url = 'https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bin/dataset.cgi?study_id=' + study_acc + '&pht=' + set_id
+	
+	#not using the urls, so we don't need these
+	#set_url = 'https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bin/study.cgi?study_id=' + study_acc 
+	#study_url = 'https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bin/dataset.cgi?study_id=' + study_acc + '&pht=' + set_id
+
 	curies.seek(0)	
 	for line in curies: #look for phenotype in ontology map
 		row = line.split('\t')
@@ -33,6 +38,6 @@ for line in infile: #iterate over infile
 			mondo = row[10]
 			efo = row[13]
 			oba = row[16]
-			out_file.write('\t'.join([variable_phv,tag_id,ncit,hpo,mondo,efo,oba,set_url,study_url]) + '\n') #output mapping
+			out_file.write('\t'.join([variable_phv,tag_id,ncit,hpo,mondo,efo,oba,variable_acc,set_acc,study_acc]) + '\n') #output mapping
 		else:
 			continue
